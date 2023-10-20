c9053e0b4c99d283923d77f5b88fca0f32922531
Garvitdudeja <68330739+Garvitdudeja@users.noreply.github.com>
2023-10-21 04:37:07 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,37 @@
+import express from 'express';
+import cors from "cors";
+import axios from 'axios';
+
+const Noob = express();
+Noob.use(cors())
+Noob.use(express.json())
+
+
+Noob.get('/',(req,res)=>{
+    res.json({"message":"updated"})
+});
+
+Noob.post('/data',async(req,res)=>{
+    try{
+        const data = req.body.data;
+        function jsonToFormData(json) {
+            const formData = new FormData();
+        
+            for (const key in json) {
+                if (json.hasOwnProperty(key)) {
+                    formData.append(key, json[key]);
+                }
+            }
+        
+            return formData;
+        }
+        const finalPostData = jsonToFormData(data);
+        const URL = "https://www.zohoapis.com/crm/v2/functions/Update_Client_Feedback_in_Deal/actions/execute?auth_type=apikey&zapikey=1003.ea52eed87a0014942321fe35b0a9b557.2958e2de5bb055884936bb746b431c82"
+        const res = await axios.post(URL,finalPostData);
+        res.json({res})
+    }catch(error){
+        res.json({"error":error.message})
+    }
+})
+
+Noob.listen(4000,()=>{console.log("Noob made api at 4000")})
